6f302a4facbb8749a1b8143a75b29fb3eff4d6cd
Naophis <nao1288stusj@gmail.com>
2023-06-27 00:01:53 +0900

    add eigen

--- main/main.cpp
+++ main/main.cpp
@@ -45,6 +45,10 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
 #include "freertos/task.h"
+
+#include <eigen3/Eigen/Eigen>
+
+// #include <Eigen/Core>
 SensingTask st;
 
 void init_uart() {
@@ -180,6 +184,43 @@ extern "C" void app_main3() {
 // second ESP_ERROR_CHECK(esp_timer_start_periodic(adc_timer, 100000)); //
 // 100m second ESP_ERROR_CHECK(esp_timer_start_periodic(adc_timer, 10000)); //
 // 10m second
+
+
+// static void multiply2Matrices()
+// {
+//     Eigen::MatrixXf M(2, 2);
+//     Eigen::MatrixXf V(2, 2);
+//     for (int i = 0; i <= 1; i++) {
+//         for (int j = 0; j <= 1; j++) {
+//             M(i, j) = 1;
+//             V(i, j) = 2;
+//         }
+//     }
+//     Eigen::MatrixXf Result = M * V;
+//     std::cout << "MatrixXf Result = " << std::endl << Result << std::endl;
+// }
+
+// static void runSVD()
+// {
+//     Eigen::MatrixXf C;
+//     C.setRandom(27, 18);
+//     Eigen::JacobiSVD<Eigen::MatrixXf> svd(C, Eigen::ComputeThinU | Eigen::ComputeThinV);
+//     Eigen::MatrixXf Cp = svd.matrixU() * svd.singularValues().asDiagonal() * svd.matrixV().transpose();
+//     Eigen::MatrixXf diff = Cp - C;
+//     std::cout << "SDV matrix U: " << std::endl << svd.matrixU() << std::endl;
+//     std::cout << "SDV singularValues: " << std::endl << svd.singularValues().transpose() << std::endl;
+//     std::cout << "SDV matrix V: " << std::endl << svd.matrixV() << std::endl;
+//     std::cout << "diff:\n" << diff.array().abs().sum() << "\n";
+// }
+
+
+// extern "C" void app_main() {
+//     std::cout << "Eigen example." << std::endl;
+//     multiply2Matrices();
+//     runSVD();
+//     std::cout << "Example finished!" << std::endl;
+// }
+
 extern "C" void app_main() {
   // Adachi adachi;
 
