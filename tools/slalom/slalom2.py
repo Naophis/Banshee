030a3167607ee6b48fe80c7cd138f7b3604af7f3
Naophis <nao1288stusj@gmail.com>
2023-08-21 21:56:56 +0900

    update

--- tools/slalom/slalom2.py
+++ tools/slalom/slalom2.py
@@ -2,6 +2,7 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 from scipy import signal
+from slalom import Slalom
 
 dt = 0.001
 m = 0.015
@@ -18,29 +19,7 @@ list_K_y = [0.5, 0.5, 0.5, 0.5]
 # list_K_y = [10, 20, 20]
 
 
-class Slalom2:
-    base_time = 0
-    v = 0
-    rad = 0
-    ang = 0
-    Et = 0
-    limit_time_count = 0
-    base_alpha = 0
-    pow_n = 0
-    start_theta = 0
-    res = {}
-    end_pos = {}
-    start_offset = 0
-    end_offset = 0
-    base_ang = 0
-    type = ""
-    start_offset_list = []
-    end_offset_list = []
-    turn_offset = {"x": 0, "y": 0}
-    cell_size = 90
-    half_cell_size = 45
-    slip_gain = 50
-    list_K_y = []
+class Slalom2(Slalom):
 
     def __init__(self, v, rad, n, ang1, ang2, ang3, end_pos, slip_gain, type, K, list_K_y):
         self.v = v
@@ -58,18 +37,6 @@ class Slalom2:
         self.list_K_y = list_K_y
         self.pow_n = n
 
-    def calc_base_time(self):
-        c = 0
-        t1 = 0
-        tmp_dt = 0.001 / 64
-
-        while c < 10000000:
-            t1 = t1 + tmp_dt
-            if (2 * self.v / self.rad * self.Et * t1) >= self.ang:
-                self.base_time = t1
-                self.limit_time_count = t1 * 2 / dt
-                return
-            c = c + 1
 
     def calc(self, start_ang):
         res = {}
@@ -222,171 +189,11 @@ class Slalom2:
         return res
 
     def calcnormal(self, start_ang):
-        res = {}
-        res["x"] = np.array([0])
-        res["y"] = np.array([0])
-        res["alpha"] = np.array([0])
-        res["w"] = np.array([0])
-        tmp_w = 0
-        tmp_theta = 0
-        tmp_x = 0
-        tmp_y = 0
-        state = 0
-        alpha = 2 * self.v * self.v / (self.rad * self.rad * self.ang / 3)
-        while True:
-            tmp_alpha = 0
-            if state == 0:
-                tmp_alpha = alpha
-            elif state == 1:
-                tmp_alpha = 0
-            elif state == 2:
-                tmp_alpha = -alpha
-
-            tmp_w = tmp_w + tmp_alpha * dt
-            tmp_theta = tmp_theta + tmp_w * dt
-
-            tmp_x = tmp_x + self.v * \
-                    math.cos(self.start_theta + tmp_theta) * dt
-            tmp_y = tmp_y + self.v * \
-                    math.sin(self.start_theta + tmp_theta) * dt
-
-            if state == 0:
-                if tmp_theta > (self.ang / 3):
-                    state = 1
-            elif state == 1:
-                if tmp_theta > (self.ang * 2 / 3):
-                    state = 2
-            elif state == 2:
-                if tmp_w < 0:
-                    state = 3
-                if tmp_theta > (self.ang):
-                    state = 3
-
-            if state == 3:
-                break
-
-            res["x"] = np.append(res["x"], tmp_x)
-            res["y"] = np.append(res["y"], tmp_y)
-            res["alpha"] = np.append(res["alpha"], tmp_alpha)
-            res["w"] = np.append(res["w"], tmp_w)
-
-        self.res = res
-        return res
+        return 0
 
     def calc_slip_normalturn(self, start_ang):
-        res = {}
-        res["x"] = np.array([0])
-        res["y"] = np.array([0])
-        res["alpha"] = np.array([])
-        res["w"] = np.array([])
-        res["v"] = np.array([])
-        res["vx"] = np.array([])
-        res["vy"] = np.array([])
-        res["beta"] = np.array([])
-        res["acc_y"] = np.array([])
-        tmp_w = 0
-        tmp_theta = start_ang * math.pi / 180
-        tmp_x = 0
-        tmp_y = 0
-        slip_theta = 0
-        ax = 0
-        ay = 0
-        vx = self.v / 1000
-        vy = 0
-        Fx = 0
-        Fy = 0
-        beta = 0
-        s = 0
-        delta_beta = 0
-        old_beta = 0
-        state = 0
-        alpha = 2 * self.v * self.v / (self.rad * self.rad * self.ang / 3)
-        while True:
-            tmp_alpha = 0
-            if state == 0:
-                tmp_alpha = alpha
-            elif state == 1:
-                tmp_alpha = 0
-            elif state == 2:
-                tmp_alpha = -alpha
-
-            old_w = tmp_w
-
-            tmp_w = tmp_w + tmp_alpha * dt
-            tmp_theta = tmp_theta + tmp_w * dt + delta_beta
-            # Fx = 0
-
-            err = self.v / 1000 - np.sqrt(vx ** 2 + vy ** 2)
-            s = s + err
-            Fx = 100.0 * err + 0.01 * s
-
-            Fx = 0
-            v2 = np.sqrt(vx ** 2 + vy ** 2)
-            tmpK = np.interp(v2 * 1000, list_K_x, self.list_K_y)
-            Fy = -tmpK * beta
-            # print(Fy, tmpK, v2)
-            ax = Fx / m + old_w * vy
-            ay = Fy / m - old_w * vx
-
-            vy = vy + ay * dt
-            vx = vx + ax * dt
-
-            tmp_v = np.sqrt(vx ** 2 + vy ** 2)
-
-            tmp_x = tmp_x + tmp_v * 1000 * \
-                    np.cos(self.start_theta + tmp_theta) * dt
-            tmp_y = tmp_y + tmp_v * 1000 * \
-                    np.sin(self.start_theta + tmp_theta) * dt
-
-            # tmp_x = tmp_x + vx * 1000 * dt
-            # tmp_y = tmp_y + vy * 1000 * dt
-
-            res["v"] = np.append(res["v"], tmp_v)
-            res["vx"] = np.append(res["vx"], vx)
-            res["vy"] = np.append(res["vy"], vy)
-
-            res["x"] = np.append(res["x"], tmp_x)
-            res["y"] = np.append(res["y"], tmp_y)
-            res["alpha"] = np.append(res["alpha"], tmp_alpha)
-            res["w"] = np.append(res["w"], tmp_w)
-            res["acc_y"] = np.append(res["acc_y"], (tmp_v * tmp_w))
-            old_beta = beta
-            beta = np.arctan2(vy, vx)
-            beta = (old_beta / dt - tmp_w) / (1.0 / dt + K / tmp_v)
-            res["beta"] = np.append(res["beta"], beta)
-
-            delta_beta = beta - old_beta
-
-            if state == 0:
-                if tmp_theta > (self.ang / 3):
-                    state = 1
-            elif state == 1:
-                if tmp_theta > (self.ang * 2 / 3):
-                    state = 2
-            elif state == 2:
-                if tmp_w < 0:
-                    state = 3
-                if tmp_theta > (self.ang):
-                    state = 3
-
-            if state == 3:
-                break
-
-        self.res = res
-
-        return res
-
-    def calc_neipire(self, t, s, N):
-        z = 1
-        t = t / s
-        P = math.pow((t - z), N - z)
-        Q = P * (t - z)
-        res = -N * P / ((Q - z) * (Q - z)) * \
-              math.pow(math.exp(1), z + z / (Q - z)) / s
-        if t == 0:
-            return 0
-        return res
-
+        return 0
+    
     def calc_offset_dist(self, start_pos_x, start_pos_y, type, prev_offset, after_offset):
         a = math.sin(self.ang)
         b = math.cos(self.ang)
